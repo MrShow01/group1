4c74c24561f1202501e034ca78fde9054d4e0973
Youssef Ahmed <ybdalzym831@gmail.com>
2025-11-07 03:08:40 +0200

    sdk in bulid gradle and Firebase.initializeApp in main

--- lib/main.dart
+++ lib/main.dart
@@ -8,10 +8,10 @@ import 'firebase_options.dart';
 
 Future<void> main() async {
 
-  // WidgetsFlutterBinding.ensureInitialized();
-  // await Firebase.initializeApp(
-  //   options: DefaultFirebaseOptions.currentPlatform,
-  // );
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp( MultiProvider(providers: [
     ChangeNotifierProvider(create: (context) => PagenavigatorNotifier())
   ], child: const MyApp()));
